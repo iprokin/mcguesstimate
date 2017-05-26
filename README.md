5cfa4b52b173a1c92c5817ac95d7ed3a701d97e0
Ilya Prokin <isprokin@gmail.com>
2017-05-26 12:25:05 +0200

    With external model

---
+++ README.md
@@ -0,0 +1,45 @@
+# Monte-Carlo Guesstimate with confidence
+
+From wikipedia:
+
+> "A back-of-the-envelope calculation is a rough calculation, typically jotted down on any available scrap of paper such as the actual back of an envelope. It is more than a guess but less than an accurate calculation or mathematical proof."
+
+Instead of coming up with a single-value estimate, this app generates distribution and draws interval covering 95% of the distribution.
+As an input, it takes a function that computes a quantity to estimate.
+Each argument of this function is specified by its distribution.
+Monte-Carlo simulations are performed to come up with a distribution of the output quantity.
+
+
+This software is similar to
+<https://github.com/getguesstimate/guesstimate-app>, but simpler, uses python
+and has programmatic interface instead of graphical.
+
+# Dependencies
+
+* python3
+* numpy
+* matplotlib
+
+# Usage
+
+Use as a commandline tool
+```{.bash}
+python gst.py ./default_model.py
+```
+
+Copy and modify `./default_model.py`.
+Run with your modified file. Profit?
+
+# Gallery for `default_model.py`
+
+![](Annual_income.png)
+![](Fraction_of_income_receiving_people.png)
+![](Population.png)
+![](Result_(GDP).png)
+
+# References
+
+* <https://en.wikipedia.org/wiki/Guesstimate>
+* <https://en.wikipedia.org/wiki/Back-of-the-envelope_calculation>
+* <https://en.wikipedia.org/wiki/Fermi_problem>
+
--- gst.py
+++ gst.py
@@ -1,11 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from functools import partial
+import numpy.random as npr
 
 
-#def tgauss_smpl(mu, sigma, size=1000):
-#    X = np.random.normal(loc=mu, scale=sigma, size=size)
-#    X[X<=0] = X[X>0].min()
-#    return X
+def file_to_str(filepath):
+    with open(filepath, "r") as myfile:
+        data = myfile.read()
+    return data
 
 
 def truncate_sample(distribution, thr=0, s='lower'):
@@ -22,72 +24,103 @@ def truncate_sample(distribution, thr=0, s='lower'):
     return f
 
 
-def result_summary(res):#, z=2.0):
-    print("mean +- std")
-    m, s = np.mean(res), np.std(res)
+def result_summary(res):
+    print("median +- mad")
+    m = np.median(res)
+    s = np.median(np.abs(res-m))
     print("{:.2e} +- {:.2e}".format(m, s))
     print("----")
-    print("95% confidence interval")
+    print("95% of values are inside this interval:")
     print("[{:.2e}, {:.2e}]".format(np.percentile(res, 2.5), np.percentile(res, 97.5)))
-    print("----")
-    print("95% confidence Gaussian approx (false)")
-    z = 2.0
-    sqrN = np.sqrt(len(res))
-    print("[{:.2e}, {:.2e}]".format(m-z*s/sqrN, m+z*s/sqrN))
 
 
-def monte_plot(vs, res, bins=50):
+def monte_plot(vs, res, bins=50, var_names=None, result_name=None):
     for i in range(len(vs)):
-        plt.figure()
+        f = plt.figure(figsize=(3,3))
         plt.hist(vs[i], bins=bins)
-        plt.title("variable # {}".format(i))
+        if var_names is None:
+            vn = "variable # {}".format(i)
+        else:
+            vn = var_names[i]
+        plt.title(vn)
+        f.savefig('./'+vn.replace(' ','_')+'.png', bbox_inches='tight', pad_inches=0.1)
 
-    plt.figure()
+    f = plt.figure(figsize=(3,3))
     plt.hist(res, bins=bins)
-    plt.title("Result")
+    if result_name is None:
+        rn = "Result"
+    else:
+        rn = "Result ({})".format(result_name)
+    plt.title(rn)
+    f.savefig('./'+rn.replace(' ','_')+'.png', bbox_inches='tight', pad_inches=0.1)
+
     plt.show()
 
 
 def monte_carlo_guesstimate(
         func,
-        #mus_sigs,
         distros,
-        sample_size=10000):
+        sample_size=10000,
+        var_names=None,
+        result_name=None):
 
-    #vs = [tgauss_smpl(*a, size=sample_size) for a in mus_sigs]
     vs = [d(size=sample_size) for d in distros]
 
     res = [func(*a) for a in zip(*vs)]
 
     result_summary(res)
-    monte_plot(vs, res)
+    monte_plot(vs, res, var_names=var_names, result_name=result_name)
 
     return res
 
 
-def lognormal(mean, std, **kwargs):
+def lognormal_mean_std(mean, std, **kwargs):
     x   = (std/mean)**2
     mu  = np.log(mean/np.sqrt(1+x))
     sig = np.sqrt(np.log(1+x))
     return np.random.lognormal(mu, sig, **kwargs)
 
 
-if __name__ == '__main__':
-    from functools import partial
-    from numpy.random import normal
-
-    """
+default_model={
+    'formula':
+        lambda p, f, s: p*f*s/60,
+    'distributions_for_each_parameter': [ # same order as in formula
+        truncate_sample(partial(npr.normal, 150*1e6, 10*1e6)),     # distribution of 1st parameter (p)
+        truncate_sample(partial(npr.normal, 0.75, 0.10)),          # distribution of 2nd parameter (f)
+        truncate_sample(partial(lognormal_mean_std, 150*1e4, 80*1e4)),  # distribution of 3rd parameter (s)
+    ],
+    'var_names': [
+        'Population',
+        'Fraction of income receiving people',
+        'Annual income'
+    ],
+    'result_name': 'GDP',
+    'description': """
     Estimate GDP of Russia as p*f*s/60, where p is population, f is fraction of
     income receiving citizens, s is annual income in RUB. "/60" converts RUB to
     USD.
     """
-    # truncate_sample is used to prevent negative values
+}
+
+
+def mcgwrapper(model):
+    print(model['description'])
+    print('-'*len(model['description'].split('\n')[1]))
     _ = monte_carlo_guesstimate(
-        lambda p,f,s: p*f*s/60,
-        [
-            truncate_sample(partial(normal, 150*1e6, 10*1e6)),     # distribution of 1st parameter (p)
-            truncate_sample(partial(normal, 0.75, 0.10)),          # distribution of 2nd parameter (f)
-            truncate_sample(partial(lognormal, 150*1e4, 70*1e4)), # distribution of 3rd parameter (s)
-        ]
+        model['formula'],
+        model['distributions_for_each_parameter'],
+        var_names=model['var_names'],
+        result_name=model['result_name']
     )
-        #[[150*1e6, 10*1e6], [0.75, 0.10], [150*1e4, 50*1e4]])
+
+
+if __name__ == '__main__':
+    import sys
+
+    model = default_model
+    if len(sys.argv)>1:
+        if sys.argv[1] in ['--help', '-h']:
+            print("Pass path to a file with model as argument. See example file.")
+        else:
+            exec(file_to_str(sys.argv[1]))
+    mcgwrapper(model)
